c932f3c8fe9dc1a445b33c99e8ce72c68c8314a3
ChristianVillalpando <cristiano.011@hotmail.com>
2016-05-24 11:54:30 -0500

    Commit 24 mayo 11)

--- js/main.js
+++ js/main.js
@@ -12,12 +12,48 @@
 });*/
 
 $( document ).ready(function() {
-	$("#div2").click(onClickElement);
+
+	$("#list").click(onClickElement);
+	$("#link").click(onLinkClick);
+	$("#div1").click(onLinkClick);
+
+	var add = $("#add");
+	var del = $("#del");
+
+	add.click(onAddClick);
+
+	del.click(onDelClick);
+
+	function onAddClick() {
+		$("#div1").removeClass("ele2");
+		 $("#div1").addClass("ele1");
+	}
+
+	function onDelClick() {
+		 $("#div1").removeClass("ele1");
+		 $("#div1").addClass("ele2");
+	}
+
 	function onClickElement () {
-		 $("#div1").css({
+		/* $("#div1").css({
 		 	"background-color":"pink",
 		 	"font-size":"2rem",
+		 });*/
+
+
+		 /* $ (this).css({
+		 	"background-color":"red",
+		 	"font-size":"3rem",
+		 });*/
+
+		$ ("#list").css({
+		 	"background-color":"blue",
+		 	"font-size":"3rem",
 		 });
 
 	};
+
+	function onLinkClick() {
+		$("[href='#']").attr("href","https://google.com");
+	}
 });
